c602bbfbe428704859d52aced7d2ca61fab2bc87
RayJi <kaikai0428@gmail.com>
2024-07-07 18:51:57 +0800

    user component click event

--- lesson-001/src/app/user/user.component.ts
+++ lesson-001/src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -8,5 +12,11 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  public selectedUser = DUMMY_USERS[randomIndex]
 
+  onSelectUser() {
+    console.log("ASD");
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
